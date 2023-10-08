a849e12c4dd9ea3ca48ce8b0449681c9479058ad
Ahmad <ahmad.jano@heig-vd.ch>
2023-10-08 15:08:25 +0200

    Add saveToFile function

--- src/main/java/ch/heigvd/Parser.java
+++ src/main/java/ch/heigvd/Parser.java
@@ -2,14 +2,13 @@ package ch.heigvd;
 
 import picocli.CommandLine.*;
 
-import java.io.File;
-import java.util.concurrent.Callable;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
 
 @Command(name = "extractmd", description = "Extracts metadata information from image files.")
 public class Parser implements Runnable {
     @Parameters(index = "0", description = "The directory containing the image files to parse.")
     private File directory;
-
 
     @Override
     public void run() {
@@ -18,6 +17,8 @@ public class Parser implements Runnable {
             return;
         }
 
+        StringBuilder output = new StringBuilder();
+
         // We could pass a filter object to listFiles() to get only image files.
         for (final File file : directory.listFiles()) {
             // Is image file
@@ -25,19 +26,32 @@ public class Parser implements Runnable {
                 javaxt.io.Image image = new javaxt.io.Image(file.getAbsolutePath());
                 java.util.HashMap<Integer, Object> exif = image.getExifTags();
 
-                System.out.println("Filename: " + file.getName());
-                System.out.println("Date: " + exif.get(0x0132));
-                System.out.println("Camera: " + exif.get(0x0110));
-                System.out.println("Manufacturer: " + exif.get(0x010F));
+                output.append("Filename: ").append(file.getName()).append('\n');;
+                output.append("Date: ").append(exif.get(0x0132)).append('\n');
+                output.append("Camera: ").append(exif.get(0x0110)).append('\n');;
+                output.append("Manufacturer: ").append(exif.get(0x010F)).append('\n');;
 
                 double[] coord = image.getGPSCoordinate();
                 if (coord != null) {
-                    System.out.println("GPS Coordinate: " + coord[0] + ", " + coord[1]);
-                    System.out.println("GPS Datum: " + image.getGPSDatum());
+                    output.append("GPS Coordinate: ").append(coord[0]).append(", ").append(coord[1]).append('\n');;
+                    output.append("GPS Datum: ").append(image.getGPSDatum()).append('\n');;
                 }
 
-                System.out.println();
+                output.append('\n');
             }
         }
+
+        System.out.println(output);
+        try {
+            saveToFile(output);
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+        }
+    }
+
+    private void saveToFile(StringBuilder output) throws IOException {
+        FileWriter writer = new FileWriter("metadata.txt", StandardCharsets.UTF_8);
+        writer.write(output.toString());
+        writer.close();
     }
 }
